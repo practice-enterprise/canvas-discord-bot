769f7ec0f4d5cc3eefbe6d6a3fac52b57c09c7c0
panda jacobs <jokke.jacobs@gmail.com>
2020-11-27 13:16:39 +0100

    Pal command

--- src/discord.ts
+++ src/discord.ts
@@ -86,6 +86,27 @@ export async function buildClient(): Promise<Client> {
       msg.channel.send(message);
     }
 
+    if (content === 'Pal') {
+      const message = new MessageEmbed()
+        .setColor('#E63F30')
+        .setTitle('Pal: Peer assisted Learning')
+        .setURL('http://bouwstenenopleiding.thomasmore.be/studenten-helpen-elkaar.html')
+        .setAuthor('Thomas More')
+        .setDescription(`
+         Peer Assisted Learning (PAL) is een verzamelterm voor allerlei strategieën die het leerproces trachten
+         te faciliteren via de actieve en interactieve tussenkomst van andere lerenden die geen professionele 
+         leerkrachten zijn. Peer Assisted Learning behelst met andere woorden een actieve leeromgeving waarin
+         peers elkaar ondersteunen en zelf verantwoordelijkheid dragen voor het eigen leer- en instructieproces (Topping, 1996).
+
+         Indien je graag PAL wilt opstarten binnen je opleiding en je hier graag ondersteuning of informatie over wilt,
+         aarzel dan niet om contact op te nemen met liesbeth.huybens@thomasmore.be.
+        
+         voor meer informatie: http://bouwstenenopleiding.thomasmore.be/studenten-helpen-elkaar.html        
+        `);
+
+      msg.channel.send(message);
+    }
+
     if (content === 'code'){
       msg.channel.send(`
 **Code block**
