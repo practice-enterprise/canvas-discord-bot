fae99288811339dc58ed4655c9177b1903e5b843
panda jacobs <jokke.jacobs@gmail.com>
2020-12-05 03:43:09 +0100

    figured out how to send a message outside of the on message event TOOK WAAAAAAAAY TOOO LONG, maybe change the as TextChannel @line 17: ./checkreminders.ts

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import { buildClient } from './discord';
+import { checkreminderinit } from './checkreminders';
 
 if (process.env.NODE_ENV == null || process.env.NODE_ENV === 'develepmont') {
   dotenv.config();
 }
 
 (async () => {
-  await buildClient();
+  const client = await buildClient();
+  checkreminderinit(client);
 })();
---
+++ src/checkreminders.ts
@@ -0,0 +1,25 @@
+
+//import { promises } from "fs";
+
+import { Client, TextChannel } from 'discord.js';
+
+//const getguild = (guildID: string, client: Client) => Promise.resolve(client.guilds?.fetch(guildID));
+
+
+export function checkreminderinit(client: Client): void {
+  setInterval(async function () {
+
+    //currently reffers to the TM testing ground + #test (channel)
+    const guildID = '780572565240414208'; //TODO get guild id from db to post in correct server
+    const channelID = '782283497582624778'; //TODO get channel of the reminder from the database
+
+    const guild = await Promise.resolve(client.guilds?.fetch(guildID));
+    const channel: TextChannel = await Promise.resolve(guild.client.channels.fetch(channelID)) as TextChannel;
+
+    //TODO check if there is a reminder that has to be sent + get the reminder message...
+    channel.send('reminder message here'); // sends a message every minute if uncommented
+
+  }, 60000);
+
+
+}
