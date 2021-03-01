145cb86a840b9d4c6259dc50dc6cef2d671892be
panda jacobs <jokke.jacobs@gmail.com>
2021-03-01 23:43:34 +0100

    Changed flow -> get all enrolements of accessable courses -> check type -> add role should work testing by TM people like teachers and other students

--- src/roleAssignment.ts
+++ src/roleAssignment.ts
@@ -1,50 +1,49 @@
 import Axios from 'axios';
-import { Client, Guild, GuildMember, Role } from 'discord.js';
+import { Client, Guild, GuildMember, Role, Structures } from 'discord.js';
 import { stringify } from 'querystring';
 import { GuildConfig } from './models/guild';
 import { GuildService } from './services/guild-service';
 
 export class RoleAssignmentService {
   static async RoleAssignmentInit(client: Client): Promise<void> {
-    const canvasToken = process.env.USERTOKEN; //canvas Personal Access Token
-    if (canvasToken == undefined) {console.error('canvasToken undefined'); return; }
+    if(process.env.USERTOKENCANVAS1 == undefined || process.env.USERTOKENCANVAS2 == undefined){console.error('canvasToken undefined');return;}
     const discordUserID = '223928391559151618';
     const serverID = '780572565240414208';
-    const rolesID: string[] = ['815954832325345350','788846220277710848', '815937311510102057']; //admin, leerkracht, student
     const guild = await client.guilds.fetch(serverID);
     
-    const canvasRoles= await this.canvasRoles(canvasToken ,await (await this.canvasUser(canvasToken)).id);
-    for(const role of canvasRoles)
+    const canvasToken: string[] = [process.env.USERTOKENCANVAS1,process.env.USERTOKENCANVAS2];//from authO flow or DB
+    const allowedValuesEnrolementType: EnrolementType[] = [{discordRoleID: '788846220277710848',typeName: 'teacher'},{ discordRoleID: '815937311510102057', typeName: 'student'}/*,'ta','observer','designer'*/]; //TODO discord role id get from a DB
+    for( const token of canvasToken)
     {
-      if(role.id == 9)//admin id
+      // eslint-disable-next-line no-await-in-loop
+      const courses = await this.canvasCourses(token);
+      console.log(courses);
+      for(const course of courses)
       {
-        this.giveRole(guild, discordUserID, rolesID[0]); //TODO go over all of the roles to check + get from DB
+        course.enrollments.forEach(element =>{
+          for(const enrolType of allowedValuesEnrolementType)
+          {
+            if(element.type == enrolType.typeName)
+            {
+              this.giveRole(guild, discordUserID, enrolType.discordRoleID);
+            }
+          }
+        });
       }
-    }  
+    }
   }
-  static async canvasUser(token: string): Promise<canvasUser> {
-    return Axios.request<canvasUser>({
+  
+  static async canvasCourses(token: string): Promise<Course[]> {
+    return Axios.request<Course[]>({
       method: 'GET',
       baseURL: process.env.CANVAS_URL,
-      url: '/api/v1/users/self',
+      url: '/api/v1/courses/',
       headers: {
         Authorization: `Bearer ${token}`
       }
     }).then((res) => { return res.data; });
   }
-  
-  static async canvasRoles(token: string, userID: number): Promise<RoleCanvas[]> {
-    return Axios.request<RoleCanvas[]>({
-      method: 'GET',
-      baseURL: process.env.CANVAS_URL,
-      url: `/api/v1/accounts/${userID}/roles`,
-      headers: {
-        Authorization: `Bearer ${token}`,
-        state: 'active'
-      }
-    }).then((res) => { return res.data; });
-  }
-  
+    
   static async giveRole(guild: Guild, discordUserID:string, roleID: string): Promise<void>{
     const userBoy = await guild?.members.fetch(discordUserID);
     const role = await guild.roles.fetch(roleID);
@@ -53,17 +52,25 @@ export class RoleAssignmentService {
       userBoy.roles.add(role);
     }
   }
+} 
+
+
+
+interface EnrolementType{
+  discordRoleID: string,
+  typeName: string
 }
 
-export interface canvasUser {
-  id: number,
-  name: string
-}
-
-export interface RoleCanvas {
-  id: number,
+interface Enrolement{
+  type: string,
   role: string,
-  label: string
+  role_id: number,
+  user_id: number
+}
+
+interface Course{
+  id: number,
+  enrollments: Enrolement[]
 }
 
 //extra functionele code
