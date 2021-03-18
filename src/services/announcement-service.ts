3356938bd77734946545c78cf855b7be66bb58a0
panda jacobs <jokke.jacobs@gmail.com>
2021-03-18 03:08:29 +0100

    created set channel and setCategoryID for the

--- src/services/announcement-service.ts
+++ src/services/announcement-service.ts
@@ -2,13 +2,14 @@
 import { CategoryChannel, Guild } from 'discord.js';
 import { GuildConfig } from '../models/guild';
 import { CanvasService } from './canvas-service';
+import { GuildService } from './guild-service';
 
 export async function createCourseChannels(dcID: string, guild: Guild, guildConfig: GuildConfig): Promise<void> {
   let parent = await guild.channels.cache.get(guildConfig.courseChannels.CategoryID) as CategoryChannel;
 
   if (parent == undefined) {
     parent = await guild.channels.create('subjects', { type: 'category' });
-    //TODO post to DB
+    GuildService.setCategoryID(parent.id, guildConfig._id);
   }
 
   if (process.env.USERTOKENCANVAS1 != undefined) {
@@ -16,13 +17,16 @@ export async function createCourseChannels(dcID: string, guild: Guild, guildConf
     if (Array.isArray(courses)) {
       for (const course of courses) {
         if (guild.channels.cache.get(guildConfig.courseChannels.channels[course.id]) == undefined) {
-          await guild.channels.create(course.name, { type: 'text', parent: parent.id });
-          //TODO post to DB
+          const newChannel = await guild.channels.create(course.name, { type: 'text', parent: parent.id });
+          GuildService.setChannel(course.id.toString(), newChannel.id, guildConfig._id);
         }
       }
+      
     }
   }
 }
+
+
 
 
 
