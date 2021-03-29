151c054b6755bdb96cfc117b7a6ee98e29264bd7
DrunkenToast <peter5leconte@gmail.com>
2021-03-29 14:27:01 +0200

    undefined

--- src/services/user-service.ts
+++ src/services/user-service.ts
@@ -2,20 +2,26 @@ import Axios from 'axios';
 import { User } from '../models/users';
 
 export class UserService {
-  static async getForDiscord(discordID: string): Promise<User> {
+  static async getForDiscord(discordID: string): Promise<User | undefined> {
     return Axios.request<User>({
       method: 'GET',
       baseURL: process.env.API_URL,
-      url: `/users/discord/${discordID}`
-    }).then((res) => res.data);
+      url: `/users/discord/${discordID}`,
+      validateStatus: () => true,
+    }).then((res) => {
+      return res.status === 200 ? res.data : undefined;
+    });
   }
 
-  static async getForCanvas(canvasID: string): Promise<User> {
+  static async getForCanvas(canvasID: string): Promise<User | undefined> {
     return Axios.request<User>({
       method: 'GET',
       baseURL: process.env.API_URL,
-      url: `/users/canvas/${canvasID}`
-    }).then((res) => res.data);
+      url: `/users/canvas/${canvasID}`,
+      validateStatus: () => true,
+    }).then((res) => {
+      return res.status === 200 ? res.data : undefined;
+    });
   }
 
   static async getForCourse(courseID: string): Promise<User | undefined> {
@@ -25,12 +31,7 @@ export class UserService {
       url: `/users/course/${courseID}`,
       validateStatus: () => true,
     }).then((res) => {
-      if (res.status === 200) {
-        return res.data;
-      }
-      else {
-        return undefined;
-      }
+      return res.status === 200 ? res.data : undefined;
     });
   }
 
