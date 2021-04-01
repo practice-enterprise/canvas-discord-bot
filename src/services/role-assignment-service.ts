02f38bb24e2a9edd0182f40e81c3ab61f4670550
panda jacobs <jokke.jacobs@gmail.com>
2021-04-02 01:39:28 +0200

    using the user db to get the courses and ids for the role assignments

--- src/services/role-assignment-service.ts
+++ src/services/role-assignment-service.ts
@@ -5,22 +5,26 @@ import { GuildService } from './guild-service';
 
 export class RoleAssignmentService {
   static async RoleAssignmentInit(client: Client): Promise<void> {
-    if (process.env.CANVAS_URL == undefined) { console.error('CANVAS_URL undefined'); return; }
-    const discordUserID = '223928391559151618';
-    const serverID = '780572565240414208';
+    const idCourses = await CanvasService.updateRoles();
+    const serverID = '780572565240414208'; //see *
     const guild = await client.guilds.fetch(serverID);
     const config = await GuildService.getForId(serverID);
-    const courses = await CanvasService.getCourses(process.env.CANVAS_URL, discordUserID);
-    if (courses == undefined) { console.error('courses undefined'); return; }
 
+    const shouldHave: string[] = [];
 
     // eslint-disable-next-line no-await-in-loop
-    for (const course of courses) {
-      course.enrollments.forEach(element => {
-        this.giveRole(guild, discordUserID, config.roles[element.type]);
-      });
+    for (const idCourse of idCourses) {
+      //TODO get all guilds of the bot for this user *
+      for (const course of idCourse.courses)
+        course.enrollments.forEach(element => {
+          if (!shouldHave.includes(element.type)) {
+            shouldHave.push(element.type);
+          }
+        });
+      for (const roleType of shouldHave) {
+        this.giveRole(guild, idCourse.id, config.roles[roleType]);
+      }
     }
-
   }
 
   static async giveRole(guild: Guild, discordUserID: string, roleID: string): Promise<void> {
@@ -31,14 +35,15 @@ export class RoleAssignmentService {
     }
   }
 
-  static async deleteRoles(client: Client):Promise<void> {
+  static async deleteRoles(client: Client): Promise<void> {
     const discordUserID = '223928391559151618';
     const serverID = '780572565240414208';
     if (process.env.CANVAS_URL == undefined) { console.error('CANVAS_URL undefined'); return; }
     const guild = await client.guilds.fetch(serverID);
     const user = await guild.members.fetch(discordUserID);
     const config = await GuildService.getForId(serverID);
-    const courses = await CanvasService.getCourses(process.env.CANVAS_URL, discordUserID);
+    const courses = await CanvasService.getCourses('a40d37b54851efbcadb35e68bf03d698', discordUserID);
+    console.log(courses);
     if (courses == undefined) { console.error('courses undefined'); return; }
     const shouldHave: string[] = [];
 
