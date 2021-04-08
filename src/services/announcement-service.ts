9e40de115982e73cfd4b843d51e0788499e56112
DrunkenToast <peter5leconte@gmail.com>
2021-04-09 00:21:09 +0200

    fix: no cache >:(

--- src/services/announcement-service.ts
+++ src/services/announcement-service.ts
@@ -2,8 +2,8 @@ import { Client, MessageEmbed } from 'discord.js';
 
 export class AnnouncementService {
   static async postAnnouncement(announcement: any, client: Client): Promise<void> {
-    const channel = await client.channels.cache.get(announcement.channelID);
-    if (channel !== undefined && channel.isText()) {
+    const channel = await client.channels.fetch(announcement.channelID);
+    if (channel.isText()) {
       channel.send(new MessageEmbed(announcement.embed));
     }
   }
