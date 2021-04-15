5720440c6d198e2fe79999daa7243c40ef854bdf
panda jacobs <jokke.jacobs@gmail.com>
2021-04-15 22:22:51 +0200

    refactor: remove unnecessarychecks

--- src/services/role-assignment-service.ts
+++ src/services/role-assignment-service.ts
@@ -4,15 +4,8 @@ import { RoleUpdateData } from '../models/role-update-data';
 
 export class RoleAssignmentService {
   static async updateRoles(data: RoleUpdateData, client: Client): Promise<void> {
-    const guild = await client.guilds.fetch(data.guildID).catch((err) => console.log(err));
-    if (!guild) { 
-      throw new Error(`could get guild: ${data.guildID}`);
-    }
-
-    const member = await guild.members.fetch({ 'user': data.userID, 'cache': true, 'force': true }).catch((err) => console.log(err))/*cache.get(data.userID)*/;
-    if (!member) { 
-      throw new Error(`could not get member: ${data.userID}`);
-    }
+    const guild = await client.guilds.fetch(data.guildID);
+    const member = await guild.members.fetch({ 'user': data.userID, 'cache': true, 'force': true });
 
     const roleList = member.roles.cache.map((role) => role.id);
 
@@ -21,11 +14,11 @@ export class RoleAssignmentService {
 
       if (data.roleTypes.includes(roleType)) {
         if (!hasRole) {
-          await member.roles.add(data.configRoles[roleType]);
+          member.roles.add(data.configRoles[roleType]);
         }
       } else {
         if (hasRole) {
-          await member.roles.remove(data.configRoles[roleType]);
+          member.roles.remove(data.configRoles[roleType]);
         }
       }
     }
